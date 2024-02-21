e891151cce06da653b98c49aecb69576aa24341c
Karthi Teja Malyala <kmalyala@microsoft.com>
2024-02-21 14:29:48 +0530

    initial

---
+++ Program.cs
@@ -0,0 +1,64 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+string appConfigConnectionString = builder.Configuration.GetConnectionString("AppConfig")!;
+Console.WriteLine("App config connection String" + appConfigConnectionString);
+string appConfigPrefix = builder.Configuration["AppConfigPrefix"]!;
+Console.WriteLine("App config Prefix" + appConfigPrefix);
+
+string appUrl = builder.Configuration["AppUrl"]!;
+Console.WriteLine("app URL " + appUrl);
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection(appConfigPrefix + ":AzureAd"));
+
+
+//Console.WriteLine("App config Prefix" + clientId);
+
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+var app = builder.Build();
+
+app.UseForwardedHeaders();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
